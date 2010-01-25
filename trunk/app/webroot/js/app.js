6543fe7f15eb3cbff100743f2af1e3a61d206793
rhythmicdevil <rhythmicdevil@3befd48e-0121-11df-9f77-c71dee008b03>
2010-01-25 21:00:26 +0000

    -- added a jquery stub function for cloning ingredient rows

--- trunk/app/webroot/js/app.js
+++ trunk/app/webroot/js/app.js
@@ -1,14 +1,18 @@
 jQuery(document).ready( function() {
 	
-	$.row_clone = function(row, table)
+	$.ingredient_row_clone = function(row)
 	{
-		console.log(row);
-		$(table).append($(row).clone());
+		new_row = $(row).clone(); 
+		// Clear the inputs
+		
+		// Increment the ingredient row index		
+		
+		// Append the row back to the table
 	}
 	
 	$('input[value="Add Another"]').live('click', function(){
-		$.row_clone($('#ingredient_row'), $(this).parent('td').parent('tr').parent('tbody').parent('table'));
-		$(this).remove();
+		$.ingredient_row_clone($(this).parent('td').parent('tr'));
+		//$(this).remove(); // remove the button so we only have a button on the new row.
 	});
 	/*
 	 * When the application loads the view is hidden. This shows the application
