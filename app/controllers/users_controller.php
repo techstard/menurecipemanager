29f1d0ba2926e5a2aad254adb97b91b556e9dee0
rhythmicdevil <rhythmicdevil@gmail.com>
2011-02-20 15:18:15 +0000

    -- stubbed user creation and password retrieval

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -3,23 +3,56 @@
 class UsersController extends AppController
 {
 
-    public function  __construct()
+    public function __construct()
     {
         parent::__construct();
         $this->layout = 'user';
     }
+
     function beforeFilter()
     {
         parent::beforeFilter();
+        $this->Auth->allow('add', 'retrievePassword');
     }
 
     function login()
     {
+
     }
 
     function logout()
     {
         $this->redirect($this->Auth->logout());
+    }
+
+    /**
+     * add method
+     *
+     * @return void
+     * @access public
+     */
+    public function add()
+    {
+        if (!empty($this->data))
+        {
+
+            $this->User->create();
+var_dump($this->data);
+exit();
+            if ($this->User->save($this->data))
+            {
+                $this->flash(__('User saved.', true), array('action' => 'index'));
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    public function retrievePassword()
+    {
+        
     }
 
 }
