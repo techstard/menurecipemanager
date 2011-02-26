9d908f4c1871a17ae305e23d8787068290051137
rhythmicdevil@gmail.com <rhythmicdevil@gmail.com@3befd48e-0121-11df-9f77-c71dee008b03>
2011-02-26 14:42:25 +0000

    -- added the concept of a secret ingredient in order to create an account -- added error handling.

--- trunk/app/controllers/users_controller.php
+++ trunk/app/controllers/users_controller.php
@@ -35,13 +35,20 @@ class UsersController extends AppController
     {
         if (!empty($this->data))
         {
-            if ($this->data['User']['password'] == $this->Auth->password($this->data['User']['password_confirm']))
+            if ($this->data['User']['secret_ingredient'] == Configure::read('SECRET_INGREDIENT'))
             {
-                $this->User->create();
-                if ($this->User->save($this->data))
+                if ($this->data['User']['password'] == $this->Auth->password($this->data['User']['password_confirm']))
                 {
-                    $this->flash(__('Account Created.', true), array('action' => 'login'));
+                    $this->User->create();
+                    if ($this->User->save($this->data))
+                    {
+                        $this->flash(__('Account Created.', true), array('action' => 'login'));
+                    }
                 }
+            }
+            else
+            {
+                $this->cakeError('login', array('title' => 'Secret Ingredient', 'msg' => 'You need a secret ingredient in order to create an account.'));
             }
         }
     }
---
+++ trunk/app/views/pages/secret_ingredient.ctp
@@ -0,0 +1,1 @@
+<h3>The Secret Ingredient</h3>
