93c00a9c1f270dcc1307b28e2c26365f644fce25
rhythmicdevil <rhythmicdevil@gmail.com>
2011-12-11 13:00:38 +0000

    -- util method to lower case strings in the DB. -- util method to delete duplicates

--- code/app/controllers/app_controller.php
+++ code/app/controllers/app_controller.php
@@ -52,4 +52,132 @@ class AppController extends Controller
         $this->render('../elements/ajax_response');
     }
 
+    /**
+     * Utility method to lower case a field in a record
+     * 
+     * Accepts args in 1 of 2 forms.
+     * 
+     * 1. http://recipe-manager/ingredients/set_to_lower_case/field:ingredient
+     * 
+     * Will update Model.ingredient
+     * 
+     * 2. http://recipe-manager/recipes/set_to_lower_case/field:ingredients.ingredient
+     * 
+     * Will update Model[field][n]['ingredient']
+     * 
+     */
+    public function set_to_lower_case()
+    {
+        if (empty($this->params['named']['field']))
+        {
+            echo 'You must enter a field to work with. recipe-manager/ingredients/set_to_lower_case/field:ingredient';
+            exit();
+        }
+
+        if (strpos($this->params['named']['field'], '.'))
+        {
+            $fields = explode('.', $this->params['named']['field']);
+        }
+        else
+        {
+            $fields[] = $this->params['named']['field'];
+        }
+
+        var_dump($fields);
+
+        $params = array(
+                //'conditions' => array('Model.field' => $thisValue), //array of conditions
+                //'recursive' => 1, //int
+                //'fields' => array($this->modelClass . '.' . $field), //array of field names
+                //'order' => array('Model.created', 'Model.field3 DESC'), //string or array defining order
+                //'group' => array('Model.field'), //fields to GROUP BY
+                //'limit' => n, //int
+                //'page' => n, //int
+                //'offset' => n, //int
+                //'callbacks' => true //other possible values are false, 'before', 'after'
+        );
+
+        $result = $this->{$this->modelClass}->find('all', $params);
+
+        foreach ($result as &$item)
+        {
+            var_dump(count($fields));
+            if (count($fields) == 1)
+            {
+                var_dump('Single Item');
+                var_dump($item[$this->modelClass][$fields[0]]);
+                $item[$this->modelClass][$fields[0]] = strtolower($item[$this->modelClass][$fields[0]]);
+                var_dump($item[$this->modelClass][$fields[0]]);
+            }
+            else if (count($fields) == 2)
+            {
+                var_dump('Nested Item');
+                foreach ($item[$this->modelClass][$fields[0]] as &$array)
+                {
+                    var_dump($array[$fields[1]]);
+                    $array[$fields[1]] = strtolower($array[$fields[1]]);
+                    var_dump($array[$fields[1]]);
+                }
+            }
+            var_dump($item);
+
+            if (!$this->{$this->modelClass}->save($item))
+            {
+                exit('Error');
+            }
+        }
+        exit('Done');
+    }
+
+    /*
+     * Util method to delete duplicate records by a field. Given the following
+     * argument:
+     * http://recipe-manager/ingredients/set_to_lower_case/field:ingredient
+     * 
+     * Will search for records Model.ingredient
+     * 
+     * If the search returns an array of more than one item then all but the first
+     * item will deleted from the DB.
+     * 
+     * 
+     */
+
+    public function delete_duplicates()
+    {
+        if (empty($this->params['named']['field']))
+        {
+            echo 'You must enter a field to work with. http://recipe-manager/ingredients/delete_duplicates/field:ingredient';
+            exit();
+        }
+
+        $field = $this->params['named']['field'];
+
+        var_dump($field);
+
+        $params = array();
+        $result = $this->{$this->modelClass}->find('all', $params);
+
+
+        foreach ($result as $item)
+        {
+            $params = array(
+                    'conditions' => array($this->modelClass . '.' . $field => $item[$this->modelClass][$field]), //array of conditions
+            );
+            $r = $this->{$this->modelClass}->find('all', $params);
+
+            if(count($r) == 2)
+            {
+                 while(count($r) > 1)
+                 {
+                     $t = array_pop($r);
+                     var_dump($t[$this->modelClass]['_id']);
+                     $this->{$this->modelClass}->delete($t[$this->modelClass]['_id']);
+                 }
+            }
+        }
+
+
+        exit('done');
+    }
+
 }
